dae65779deb5ea1b419cfe7f6dfc5aa61321b6a2
andersonjr667 <alsj1520@gmail.com>
2025-06-03 14:22:35 -0300

    Teste proxy reverso

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/', createProxyMiddleware({
+  target: 'https://3f7c-177-39-9-218.ngrok-free.app', // seu link do Ngrok
+  changeOrigin: true,
+  secure: false,
+}));
+
+app.listen(3000, () => {
+  console.log('Proxy rodando na porta 3000');
+});
