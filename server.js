79f3cdc8d0cfae8a9bb58f04c184e76ce7341d82
Anderson Jr <140277030+andersonjr667@users.noreply.github.com>
2025-06-03 21:56:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use('/', createProxyMiddleware({
-  target: 'https://674c-2804-1b3-6147-29cd-7415-df8c-eb77-c92b.ngrok-free.app/', // seu link do ngrok aqui
+  target: 'https://674c-2804-1b3-6147-29cd-7415-df8c-eb77-c92b.ngrok-free.app', // seu link do ngrok aqui
   changeOrigin: true,
   secure: false,
 }));
